535e2b6473cc17780e3a4e7cf0e3a4ca5a5c0f3c
zimu <1792438326@qq.com>
2026-04-10 11:16:48 +0800

    Initial commit

---
+++ src/main/java/com/zimu/imdemo/ImDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zimu.imdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zimu/imdemo/ImDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zimu.imdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
